9f13616a9dddc913249ed1c7abf6ab4563217cec
QiangRO <aroch22222@hotmail.com>
2021-11-28 20:12:35 -0400

    Se agrega subnivele a la pagina, se adjunta codigo en un solo css

--- js/lugares.js
+++ js/lugares.js
@@ -5,43 +5,42 @@ const copy = document.querySelector(`.copy`);
 const closemodal = document.querySelector(`.bx.bx-x`);
 
 images.forEach(image => {
-    image.addEventListener(`click`,()=>{
-        addImage(image.getAttribute(`src`),image.getAttribute(`alt`))
+    image.addEventListener(`click`, () => {
+        addImage(image.getAttribute(`src`), image.getAttribute(`alt`))
     })
 })
 
-const addImage = (src, alt) =>{
+const addImage = (src, alt) => {
     containerImage.classList.toggle(`move`);
     imageContainer.classList.toggle(`show`);
     imageContainer.src = src;
     copy.innerHTML = alt;
 }
 
-closemodal.addEventListener(`click`, ()=>{
+closemodal.addEventListener(`click`, () => {
     containerImage.classList.toggle(`move`);
     imageContainer.classList.toggle(`show`);
 })
 
 /* */
-$(document).ready(function(){
-          
-    $(".content-box").click(function(){
-       
+$(document).ready(function () {
+    $(".content-box").click(function () {
         $(".content-box").animate({
-            width:`300px`,
-            height:`100px`
+            width: `300px`,
+            height: `100px`
         }, 1000);
         $(".content-after").show().animate({
-            opacity: "1"}, 3000);
+            opacity: "1"
+        }, 3000);
     });
 
 });
 
-function mostrarPremio(){
-    document.getElementById(`premio`).style.display=`block`;
+function mostrarPremio() {
+    document.getElementById(`premio`).style.display = `block`;
 }
-function ocultarPremio(){
-    document.getElementById(`premio`).style.display=`none`;
+function ocultarPremio() {
+    document.getElementById(`premio`).style.display = `none`;
 }
 
 
